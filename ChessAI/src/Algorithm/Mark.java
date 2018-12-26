bd2cad13250c99542133caeb4ca6c2e2a333d446
Yeeee19 <qaz45685274@gmail.com>
2018-12-27 05:33:02 +0800

    12/27 1st modify

--- ChessAI/src/Algorithm/Mark.java
+++ ChessAI/src/Algorithm/Mark.java
@@ -1,5 +1,40 @@
 package Algorithm;
 
 public class Mark {
+	
+	private enum MarkType{}
+	
+	public Mark getMarkTake() {
+		
+	}
+	
+	public int getWhiteScore() {
+		
+	}
 
+	public int getBlackScore() {
+		
+	}
+	
+	public int getWhiteScore(Board b) {
+		
+	}
+	
+	public int getBlackScore(Board b) {
+		
+	}
+	
+	
 }
+
+
+
+/*
+	Mark¾Ö¦³¤U´Ñªº¼Ð°O¡A¦³¤@­Óenum¡A¥]§t¤T­ÓªF¦è¡AMark.white, Mark.black, Mark.empty
+ƒÞ	private enum MarkType{stop = 1, sit, stand, walk, run} type;
+ƒÞ	public Mark getMarkTake() ¨ú±o²{¦b¤U´Ñªºª±®a¡A¥u·|¦^¶ÇMark.white, Mark.black
+ƒÞ	public int getWhiteScore() ¨ú±o·í«e¥Õ¤lªº¤À¼Æ
+ƒÞ	public int getBlackScore() ¨ú±o·í«e¶Â¤lªº¤À¼Æ
+ƒÞ	public int getWhiteScore(Board b) ·í½L­±¬°b¡A¥Õ¤lªº¤À¼Æ
+ƒÞ	public int getWhiteScore(Board b) ·í½L­±¬°b¡A¶Â¤lªº¤À¼Æ 
+*/
